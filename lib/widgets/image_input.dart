fb7024babaf97b38b78cf550bbdb823c9d07ef4b
macbook <a7med.assem@gmail.com>
2020-02-10 15:47:02 +0200

    Storing the image on the device

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,6 +2,10 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+
+
 
 class ImageInput extends StatefulWidget {
   @override
@@ -11,7 +15,13 @@ class ImageInput extends StatefulWidget {
 class _ImageInputState extends State<ImageInput> {
   File _storedImage;
   Future<void> _takePicture() async {
-    final imageFile = await ImagePicker.pickImage(source: ImageSource.camera,maxWidth: 600);
+    final imageFile = await ImagePicker.pickImage(source: ImageSource.gallery,maxWidth: 600);
+    setState(() {
+      _storedImage = imageFile;
+    });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await imageFile.copy('${appDir.path} /$fileName');
   }
   @override
   Widget build(BuildContext context) {
